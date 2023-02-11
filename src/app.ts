c17ba0da88d347e1063724bebe72ac39d477791f
Zwirek_Dev <glanzgarage@gmail.com>
2023-02-11 12:04:35 +0100

    implement auth by express-session . login, singin, logout

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,34 @@ import "dotenv/config";
 import express from "express";
 import { NextFunction, Request, Response } from "express-serve-static-core";
 import notesRoutes from "./routes/notes";
+import userRoutes from "./routes/users";
 import morgan from "morgan";
 import createHttpError, { isHttpError } from "http-errors";
+import session from "express-session";
+import env from "./util/validateEnv";
+import MongoStore from "connect-mongo";
 
 const app = express();
 
 app.use(morgan("dev"));
 
 app.use(express.json());
+
+app.use(
+  session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000,
+      // onehour
+    },
+    rolling: true,
+    store: MongoStore.create({ mongoUrl: env.MONGO_CONNECTION_STRING }),
+  })
+);
+
+app.use("/api/users", userRoutes);
 
 app.use("/api/notes", notesRoutes);
 
